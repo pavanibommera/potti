42dc12a059e687237f6f9f99c294c71e76af10ec
PAVANI B <PAVANI B@LAPTOP-7VMGP23T>
2022-02-04 16:04:04 +0530

    third commit

---
+++ java1/comjavacomments/Example1.java
@@ -0,0 +1,22 @@
+package comjavacomments;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		/**
+		 * Write a java program to print customer information(id,name and address)
+		 */
+		int CustomerId=1234;
+		String CustomerName="sunitha";
+		String CustomerAddress="Hyderabad";
+		int Customersalary=50000;
+		System.out.println(CustomerId);
+		System.out.println(CustomerName);
+		System.out.println(CustomerAddress);
+		System.out.println(Customersalary);
+		
+
+	}
+
+}
